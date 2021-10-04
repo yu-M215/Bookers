c7f2ea1d378ccd2197305c2516dc25221b9a0e3b
yu-M215 <yuu0609km.215@gmail.com>
2021-10-04 09:16:16 +0000

    [Add] UpdateアクションとDestroyアクションの追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -17,6 +17,18 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to books_path
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
   private
 
   def book_params
