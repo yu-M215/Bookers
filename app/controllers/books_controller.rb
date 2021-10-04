8bbc6555289bc5ec2bb7ab53496e6531845447c5
yu-M215 <yuu0609km.215@gmail.com>
2021-10-04 09:30:10 +0000

    [Add] サクセスメッセージの表示設定追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,6 +7,8 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
+    redirect_to book_path(book.id)
+    flash[:notice] = "Book was successfully created."
   end
 
   def show
@@ -20,13 +22,15 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to books_path
+    redirect_to book_path
+    flash[:notice] = "Book was successfully updated."
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
     redirect_to books_path
+    flash[:notice] = "Book was successfully destroyed."
   end
 
   private
