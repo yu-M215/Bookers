01c32503f5d5ea8f200527a9793e6032b5712f70
yu-M215 <yuu0609km.215@gmail.com>
2021-10-05 06:04:49 +0000

    [Update] update時エラーメッセージ表示追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -24,10 +24,13 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path
-    flash[:notice] = "Book was successfully updated."
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path
+      flash[:notice] = "Book was successfully updated."
+    else
+      render :edit
+    end
   end
 
   def destroy
